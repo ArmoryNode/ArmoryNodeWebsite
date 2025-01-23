a183509255298db7d85956c3fe733657ddedb2db
ArmoryNode <22787155+ArmoryNode@users.noreply.github.com>
2025-01-23 14:14:38 -0800

    Fixed build script

--- build.ts
+++ build.ts
@@ -2,9 +2,20 @@ import { compileToString as ElmCompile } from "https://deno.land/x/deno_elm_comp
 import { minify } from "npm:terser";
 import * as sass from "npm:sass";
 
-const maxRetries = 3;
+async function createDirectories() {
+    await Deno.mkdir("./static/css", { recursive: true }).catch(error => {
+        if (error instanceof Deno.errors.AlreadyExists)
+            return;
+        throw error;
+    });
+    await Deno.mkdir("./static/js", { recursive: true }).catch(error => {
+        if (error instanceof Deno.errors.AlreadyExists)
+            return;
+        throw error;
+    });
+}
 
-async function compileAndMinifyElm(retries: number = 0) {
+async function compileAndMinifyElm() {
     try {
         const compiledJs = await ElmCompile("./src/Main.elm", { mode: "optimize" });
         const minifiedJs = await minify(compiledJs);
@@ -14,40 +25,20 @@ async function compileAndMinifyElm(retries: number = 0) {
             if (error.message.includes("Failed to spawn 'elm': entity not found")) {
                 throw "Elm is not installed. Please install Elm from https://guide.elm-lang.org/install/";
             }
-
-            Deno.mkdir("./static/js", { recursive: true });
-
-            if (retries < maxRetries) {
-                compileAndMinifyElm(retries + 1);
-            } else {
-                console.error("Failed to compile Elm after 3 retries");
-            }
         }
 
         throw error;
     }
 }
 
-async function compileSass(retries: number = 0) {
-    try {
-        const compiledCss = sass.compile("./src/assets/styles/main.scss", {
-            style: "compressed",
-            sourceMap: false
-        });
-        await Deno.writeTextFile("./static/css/main.min.css", compiledCss.css);
-    } catch (error) {
-        if (error instanceof Deno.errors.NotFound) {
-            Deno.mkdir("./static/css", { recursive: true });
-
-            if (retries < maxRetries) {
-                compileSass(retries + 1);
-            } else {
-                console.error("Failed to compile Sass after 3 retries");
-            }
-        }
-
-        throw error;
-    }
+async function compileSass() {
+    const compiledCss = sass.compile("./src/assets/styles/main.scss", {
+        style: "compressed",
+        sourceMap: false
+    });
+    await Deno.writeTextFile("./static/css/main.min.css", compiledCss.css, {
+        create: true
+    });
 }
 
 // Only run if the script is run directly, not imported as a module
@@ -56,16 +47,13 @@ if (import.meta.main) {
 }
 
 export async function compile() {
-    try {
-        console.log("🎨 Compiling Sass...");
-        await compileSass();
-    
-        console.log("🌳 Compiling Elm...");
-        await compileAndMinifyElm();
-    
-        console.log("✅ Done!");
-    } catch (error) {
-        console.log(error);
-        Deno.exit(1);
-    }
+    await createDirectories();
+
+    console.log("🎨 Compiling Sass...");
+    await compileSass();
+
+    console.log("🌳 Compiling Elm...");
+    await compileAndMinifyElm();
+
+    console.log("✅ Done!");
 }
